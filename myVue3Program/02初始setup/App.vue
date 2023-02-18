6185fb5e9cb488e2884f9c8e87d286c6564cce80
jasmine <387951031@qq.com>
2023-02-18 21:43:32 +0800

    230218 学习vue3的第一天晚上（在杭电） set up

---
+++ myVue3Program/02初始setup/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>我是app组件</h1>
+  <h2>姓名：{{name}}</h2>
+  <h3>年龄：{{age}}</h3>
+  <h3>一个人的信息</h3>
+  <button @click="sayHello">点我</button>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  setup(){
+    //数据
+    let name ='张三'
+    let age =18
+
+    //方法
+    function sayHello(){
+      alert(`我叫${name}`)
+    }
+
+    return{
+      name,
+      age,
+      sayHello
+    }
+
+
+  }
+}
+</script>
